ef54524cec742eefbe07b71d1ce2e44b977ea1ec
Michael Jordan <michdan25@gmail.com>
2021-07-30 17:25:14 +1200

    scripts

--- Assets/Scripts/CropScript.cs
+++ Assets/Scripts/CropScript.cs
@@ -6,22 +6,32 @@ public class CropScript : MonoBehaviour
 {
     [Header("InputInfo")]
     public int m_growthPeriod;
+    public GameObject m_plant;
+    public Vector3 m_finalSize;
+    private Vector3 m_stepSize;
 
     [Header("ReadOnly")]
     public int m_birthDay = 0;
     public int m_nextHarvest = 0;
 
+    private int m_lastRecordedDay = 0;
     // Start is called before the first frame update
     void Start()
     {
         m_birthDay = GameManager.instance.m_day;
         m_nextHarvest = m_birthDay + m_growthPeriod;
+
+        m_stepSize = m_finalSize / m_growthPeriod;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(m_lastRecordedDay < GameManager.instance.m_day)
+        {
+            m_lastRecordedDay = GameManager.instance.m_day;
+            StartCoroutine(GrowStep(1.0f));
+        }
     }
 
     public void Harvest()
@@ -30,5 +40,21 @@ public class CropScript : MonoBehaviour
         {
             //Harvested;
         }
+    }
+
+    public IEnumerator GrowStep(float seconds)
+    {
+        Vector3 start = m_plant.transform.localScale;
+        Vector3 target = m_plant.transform.localScale + m_stepSize;
+        
+        float time = 0.0f;
+        while(time != seconds)
+        {
+            time += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+            m_plant.transform.localScale = Vector3.Lerp(start, target, time/seconds);
+        }
+        m_plant.transform.localScale = target;
+        yield return null;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -54,6 +54,7 @@ public class GameManager : MonoBehaviour
 
     public float m_currentHour = 8;
     public int m_day = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -63,20 +64,24 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(InputManager.instance.IsKeyDown(KeyType.T))
+        {
+            SkipTime(8.0f);
+        }
     }
 
     private void InitialiseFunc()
     {
         m_items = new ItemDB(m_itemsJson.text);
+
     }
 
     public void SkipTime(float hoursIncreased)
     {
         m_currentHour += hoursIncreased;
-        if (m_currentHour > 23)
+        if (m_currentHour >= 24)
         {
-            m_currentHour -= 23;
+            m_currentHour -= 24;
             m_day++;
         }
             
