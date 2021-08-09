b6d329402e9d6d765ca026346c7c9c86a6f2eaf5
William <55791230+GrizzBe@users.noreply.github.com>
2021-08-09 20:27:52 +1200

    gamer

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        // Delete current save.
+        LevelLoader.instance.LoadNewLevel("SceneChange2");
+    }
+    public void ContinueGame()
+    {
+        LevelLoader.instance.LoadNewLevel("SceneChange2");
+    }
+    public void Settings()
+    {
+
+    }
+    public void QuitGame()
+    {
+        LevelLoader.instance.QuitGame();
+    }
+}
